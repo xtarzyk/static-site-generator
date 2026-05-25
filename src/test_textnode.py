825e29f37e208126e6346132e1441ae1e2b2075f
xtarzyk <xtarzyk@tutanota.com>
2026-05-25 22:26:15 +0200

    some test added

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,5 +7,17 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_text_type(self):
+        node = TextNode("AAAAAAAAAAAAAAAA", TextType.BOLD)
+        node2 = TextNode("bbbbbbbbbbb", TextType.BOLD)
+        self.assertEqual(node.text_type, node2.text_type)
+
+    def test_wrong_text_type(self):
+        node = TextNode("AAAAAAAAAAAAAAAA", TextType.BOLD)
+        node2 = TextNode("bbbbbbbbbbb", TextType.ITALIC)
+        self.assertNotEqual(node.text_type, node2.text_type)
+
+    
+
 if __name__ == "__main__":
     unittest.main()
