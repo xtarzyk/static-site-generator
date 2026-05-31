059fd21c4f5eddaa72356877df1d441c944ac8c9
xtarzyk <xtarzyk@tutanota.com>
2026-05-31 18:15:55 +0200

    copy_static added in main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
-from htmlnode import HTMLNode
-from textnode import TextNode, TextType
+from os import listdir, mkdir
+from os.path import exists, isfile, join
+from shutil import copy, rmtree
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    htmlnode = HTMLNode("p", "Some text")
-    print(node)
-    print(htmlnode)
+    def copy_static(src, dst):
+        print(f"src: {src}\ndst: {dst}")
+        if exists(dst):
+            rmtree(dst)
+            mkdir(dst)
+        else:
+            mkdir(dst)
 
+        def copy_src(src, dst):
+            for entry in listdir(src):
+                src_entry = join(src, entry)
+                dst_entry = join(dst, entry)
+
+                if isfile(src_entry):
+                    copy(src_entry, dst)
+                else:
+                    mkdir(dst_entry)
+                    copy_src(src_entry, dst_entry)
+
+        copy_src(src, dst)
+                
+    copy_static("static", "public")
 if __name__ == "__main__":
     main()
