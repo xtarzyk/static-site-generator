d7a142a749aa6a2921603d1636b8013eaa0c3d5f
xtarzyk <xtarzyk@tutanota.com>
2026-05-31 16:32:00 +0200

    add text_node_to_html_node and test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -16,6 +16,12 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("AAAAAAAAAAAAAAAA", TextType.BOLD)
         node2 = TextNode("bbbbbbbbbbb", TextType.ITALIC)
         self.assertNotEqual(node.text_type, node2.text_type)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
     
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,9 @@
 from enum import Enum
 
+from src.htmlnode import LeafNode
+
 class TextType(Enum):
+    TEXT = ""
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -22,3 +25,21 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text, None)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text, None)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text, None)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text, None)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError('Wrong text type!')
+        
