c244006cc5dee183cf5b7ed931b28c5b59bfbd06
xtarzyk <xtarzyk@tutanota.com>
2026-06-05 21:19:28 +0200

    Create split_nodes_delimiter.py

---
+++ src/split_nodes_delimiter.py
@@ -0,0 +1,24 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        split_nodes = []
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Matching closing delimiter is not found")
+
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+
+        new_nodes.extend(split_nodes)
+    return new_nodes
