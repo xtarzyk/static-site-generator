ed7443526cc3a24aed80118549d172d0694e3ff6
xtarzyk <xtarzyk@tutanota.com>
2026-06-19 21:53:12 +0200

    generate_content & extract title added with tests, file refactor

--- src/main.py
+++ src/main.py
@@ -1,36 +1,11 @@
-from os import listdir, mkdir
-from os.path import exists, isfile, join
-from shutil import copy, rmtree
+from copystatic import copy_static
+from generate_content import generate_page
 
 def main():
-    def copy_static(src, dst):
-        print(f"src: {src}\ndst: {dst}")
-        if exists(dst):
-            rmtree(dst)
-            mkdir(dst)
-        else:
-            mkdir(dst)
-
-        def copy_src(src, dst):
-            for entry in listdir(src):
-                src_entry = join(src, entry)
-                dst_entry = join(dst, entry)
-
-                if isfile(src_entry):
-                    copy(src_entry, dst)
-                else:
-                    mkdir(dst_entry)
-                    copy_src(src_entry, dst_entry)
-
-        copy_src(src, dst)
-        
-    # This is code for Website section
-    def extract_title(markdown):
-        for line in markdown.readlines():
-            if line.startswith(("# ", "#")):
-                return line.strip("# ")
-        raise Exception("No h1 header found!")
     
     copy_static("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
+
+
 if __name__ == "__main__":
     main()
